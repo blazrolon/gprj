72522a3060476d05de2504b65e2559d2ed6db1f7
blazrolon <blazrolon@gmail.com>
2022-11-17 16:33:35 -0300

    spline and illustrator

---
+++ spline/src/main.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/W-e5-3p0OfOfa7Au/scene.splinecode');
